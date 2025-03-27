8a9fd831f8ac68d229d7822772e02692114e60a9
Murad Gazymagomedov <murad.dev@bk.ru>
2025-03-27 23:14:11 +0400

    css start

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("copyright_year").textContent = new Date().getFullYear();
+
+    function theme_switcher() {
+
+    }
+
+    
+});
